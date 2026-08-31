d882ee9c1140bb36a973ea963a0c9cb5b8211820
Dro <321597175+DroStacks@users.noreply.github.com>
2026-08-31 15:48:52 -0700

    Add versioning to stylesheet link in register.php

--- html/register.php
+++ html/register.php
@@ -12,7 +12,7 @@ session_start();
 
     <title>Register | HealthBridge Medical</title>
 
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css?v=2">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
 </head>
 
