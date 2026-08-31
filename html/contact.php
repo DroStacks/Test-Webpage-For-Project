19917386068c4d1e42f785a5734a6e2c3ef06fc7
Dro <321597175+DroStacks@users.noreply.github.com>
2026-08-31 15:47:42 -0700

    Add version query to stylesheet link

--- html/contact.php
+++ html/contact.php
@@ -12,7 +12,7 @@ session_start();
 
     <title>Contact | HealthBridge Medical</title>
 
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css?v=2">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
 </head>
 
