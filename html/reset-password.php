264ff773c9e8279c203fdccce2f3fd54d47164a5
Dro <321597175+DroStacks@users.noreply.github.com>
2026-08-31 15:49:09 -0700

    Add version query to stylesheet link

--- html/reset-password.php
+++ html/reset-password.php
@@ -20,7 +20,7 @@ if ($token === "") {
 
     <title>Reset Password | HealthBridge Medical</title>
 
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css?v=2">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
 </head>
 
