342947d93ed6b70f3de161b35644ca6e9fbd8847
Dro <321597175+DroStacks@users.noreply.github.com>
2026-08-30 17:04:57 -0700

    Add session handling for user login state

--- html/login.php
+++ html/login.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,13 +29,33 @@
                     <span>Language: English</span>
                 </div>
 
-                <div class="utility-right">
-                    <a href="register.php">Register</a>
+               <div class="utility-right">
 
-                    <a href="login.php" class="login-button">
-                        Patient Login
-                    </a>
-                </div>
+    <?php if (isset($_SESSION["user_id"])): ?>
+
+        <span>
+            Welcome, <?php echo htmlspecialchars($_SESSION["first_name"]); ?>
+        </span>
+
+        <a href="../php/dashboard.php">
+            Dashboard
+        </a>
+
+        <a href="../php/logout.php" class="login-button">
+            Logout
+        </a>
+
+    <?php else: ?>
+
+        <a href="register.php">Register</a>
+
+        <a href="login.php" class="login-button">
+            Patient Login
+        </a>
+
+    <?php endif; ?>
+
+</div>
 
             </div>
         </div>
