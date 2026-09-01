c60b7b7f294712612634ad0f132c8a8b1f8c69c8
Dro <321597175+DroStacks@users.noreply.github.com>
2026-08-31 17:44:46 -0700

    Update stylesheet and script version in privacy.php

--- html/privacy.php
+++ html/privacy.php
@@ -13,7 +13,7 @@ session_start();
 
     <title>Privacy Policy | HealthBridge Medical</title>
 
-    <link rel="stylesheet" href="../css/style.css?v=2">
+    <link rel="stylesheet" href="../css/style.css?v=3">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
 </head>
 
@@ -257,7 +257,9 @@ session_start();
     </footer>
 
 
-    <script src="../js/script.js"></script>
+    <?php require_once "../php/chatbot-widget.php"; ?>
+
+<script src="../js/script.js?v=4"></script>
 
 </body>
 
