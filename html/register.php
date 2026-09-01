a45b01387492bdbbc754a08d28530cf55eaca582
Dro <321597175+DroStacks@users.noreply.github.com>
2026-08-31 17:45:14 -0700

    Update stylesheet and script version in register.php

--- html/register.php
+++ html/register.php
@@ -12,7 +12,7 @@ session_start();
 
     <title>Register | HealthBridge Medical</title>
 
-    <link rel="stylesheet" href="../css/style.css?v=2">
+    <link rel="stylesheet" href="../css/style.css?v=3">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
 </head>
 
@@ -337,7 +337,9 @@ session_start();
     </footer>
 
 
-    <script src="../js/script.js"></script>
+    <?php require_once "../php/chatbot-widget.php"; ?>
+
+<script src="../js/script.js?v=4"></script>
 
 </body>
 
