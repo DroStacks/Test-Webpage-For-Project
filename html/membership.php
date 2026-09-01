3a2f45d0d89ec40be455d616e9c409e90bd8f918
Dro <321597175+DroStacks@users.noreply.github.com>
2026-08-31 17:44:20 -0700

    Update stylesheet version and add chatbot widget
    Updated stylesheet version and included chatbot widget.

--- html/membership.php
+++ html/membership.php
@@ -11,7 +11,7 @@ session_start();
 
     <title>Memberships | HealthBridge Medical</title>
 
-   <link rel="stylesheet" href="../css/style.css?v=2">
+   <link rel="stylesheet" href="../css/style.css?v=3">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
 </head>
 
@@ -362,6 +362,10 @@ session_start();
 
 </footer>
 
+    <?php require_once "../php/chatbot-widget.php"; ?>
+
+<script src="../js/script.js?v=4"></script>
+    
 </body>
 
 </html>
