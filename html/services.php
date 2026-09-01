7b0634517a0b3aa8cb22d3cfd7f7ba18ae4650af
Dro <321597175+DroStacks@users.noreply.github.com>
2026-08-31 17:46:28 -0700

    Update stylesheet and script versioning

--- html/services.php
+++ html/services.php
@@ -11,7 +11,7 @@ session_start();
 
     <title>Services | HealthBridge Medical</title>
 
-    <link rel="stylesheet" href="../css/style.css?v=2">
+    <link rel="stylesheet" href="../css/style.css?v=3">
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
 </head>
 
@@ -226,7 +226,9 @@ session_start();
     </footer>
 
 
-    <script src="../js/script.js"></script>
+    <?php require_once "../php/chatbot-widget.php"; ?>
+
+<script src="../js/script.js?v=4"></script>
 
 </body>
 
